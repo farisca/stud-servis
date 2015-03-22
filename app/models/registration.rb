4a88734093e7cc437adf0a06fb2920e94baba18b
Tajma Kovacevic <tajmakovacevic5@gmail.com>
2015-03-22 18:15:16 +0000

    Validation of field description

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,6 +3,10 @@ class Registration < ActiveRecord::Base
   belongs_to :student
   has_many:locations
   
+  #Validation of fields
+  validates :description, length: { maximum: 200 }
+  #validates :duration, 1 or 0
+  
   def getAll()
       registrations=Registration.all
       
