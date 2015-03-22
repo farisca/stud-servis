75c91ef3c321987237b5a0592fe4e691ad1f14ff
Tajma Kovacevic <tajmakovacevic5@gmail.com>
2015-03-22 18:32:01 +0000

    Services for registration, put is missing only

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -13,5 +13,21 @@ class Registration < ActiveRecord::Base
       registrations
   end
   
+  def delete (id_reg) #brisanje po registation id
+  	registation = Registration.find_by(id: id_reg)
+  	registation.destroy
+  end
+  
+  def post
+  	self.save
+  end
+  
+  def get(id_reg)
+  	registation = Registration.find_by(id: id_reg)
+
+  	registation
+  end
+  
+  
   
 end
