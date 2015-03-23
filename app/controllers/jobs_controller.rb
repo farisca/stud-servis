6041e21f2918bb35a10d92a9deb9ec8443075b5e
Faris Cakaric <fcakaric1@etf.unsa.ba>
2015-03-23 15:34:45 +0100

    Izmijenjen kontroler za poslove

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -29,10 +29,8 @@ class JobsController < ApplicationController
 
     respond_to do |format|
       if @job.save
-        format.html { redirect_to @job, notice: 'Oglas uspjesno kreiran.' }
         format.json { render :show, status: :created, location: @job }
       else
-        format.html { render :new }
         format.json { render json: @job.errors, status: :unprocessable_entity }
       end
     end
@@ -43,10 +41,8 @@ class JobsController < ApplicationController
   def update
     respond_to do |format|
       if @job.update(job_params)
-        format.html { redirect_to @job, notice: 'Job was successfully updated.' }
         format.json { render :show, status: :ok, location: @job }
       else
-        format.html { render :edit }
         format.json { render json: @job.errors, status: :unprocessable_entity }
       end
     end
@@ -57,7 +53,6 @@ class JobsController < ApplicationController
   def destroy
     @job.destroy
     respond_to do |format|
-      format.html { redirect_to jobs_url, notice: 'Job was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
