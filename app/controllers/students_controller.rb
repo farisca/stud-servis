3c414353acbb8796a2e9862b1b689e8438aa9422
Faris Cakaric <fcakaric1@etf.unsa.ba>
2015-03-23 15:57:20 +0100

    Ispravljeni kontroleri za korisnike i studente.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,31 +1,38 @@
 class StudentsController < ApplicationController
   	
   def new
-  	@student = Student.new
+    @student = Student.new
   end
 
   def create
-  	@student = Student.new(student_params)
-  	@student.post
+    @student = Student.new(student_params)
+    respond_to do |format|
+      if @student.save
+        format.json { render json: @student, status: :created, location: @student }
+      else
+        format.json { render json: @student.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
-  	@student = Student.find(params[:id])
+    @student = Student.find(params[:id])
+    render json: @student
   end
 
   def update
-  	is_updated = @student.put(user_params)
-  	render json: { error: is_updated }
+      is_updated = @student.put(user_params)
+      render json: { error: is_updated }
   end
 
   def destroy
-  	is_deleted = @student.delete(@student.id)
-  	render json: { error: is_deleted } 
+      is_deleted = @student.delete(@student.id)
+      render json: { error: is_deleted } 
   end
 
   private
     def student_params
-      params.require(:student).permit(:student_id, :name, :surname, :location, :university, :faculty, :cv, :user)
+      params.require(:student).permit(:name, :surname, :location, :university, :faculty, :cv, :user)
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,44 @@
 class UsersController < ApplicationController
-  before_action :set_user
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   
   def new
-  	@user = User.new
-  end
-
-  def create
-  	@user = User.new(user_params)
-  	@user.post
+    @user = User.new
   end
 
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params)
+ 
+    respond_to do |format|
+      if @user.save
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
-  	is_updated = @user.put(user_params)
-  	render json: { error: is_updated }
+      is_updated = @user.put(user_params)
+      render json: { error: is_updated }
   end
 
   def destroy
-  	is_deleted = @user.delete(@user.id)
-  	render json: { error: is_deleted } 
+      is_deleted = @user.delete(@user.id)
+      render json: { error: is_deleted } 
   end
 
   private
-    def user_params
-      params.require(:user).permit(:user_id, :username, :email, :role, :active)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :role, :active)
+  end
 
-   	def set_user 
-  		@user = User.find(params[:id])
-  	end
+  def set_user 
+    @user = User.find(params[:id])
+  end	
 
 end
