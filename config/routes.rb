28f11587018eed956a521de8ffd0211442e36706
Maja Kozar <maja.kozar88@gmail.com>
2015-03-23 22:46:36 +0000

    Delete of Hello World

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'employers/new'
   get 'students/new'
   get 'users/new'
   
   get 'users' => 'user#show'
   get 'students' => 'student#show'
-  get 'employers' => 'employer#show'
 
   resources :jobs
 
@@ -21,7 +19,6 @@ Rails.application.routes.draw do
 
   resources :students
 
-  resources :employers
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
