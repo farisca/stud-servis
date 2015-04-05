406d0245856545cbf40a2803e71f5cf8b23b0c52
ajdinkahrovic <kahrovic@hotmail.com>
2015-04-05 22:27:24 +0200

    registracija kompanije

--- app/assets/javascripts/ng-app/controllers/registracijaKompanije.js
+++ app/assets/javascripts/ng-app/controllers/registracijaKompanije.js
@@ -1,4 +1,27 @@
 angular.module('aplikacija')
-    .controller('login', function ($scope) {
-        $scope.things = ['Angular', 'Rails 4.1', 'Working', 'Together!!'];
-    });
+    .controller('registerCompanyController', ['$http', function ($http) {
+    	this.podaci={};
+    	this.errorMsg="";
+    	this.successMsg="";
+
+    	this.isError=function() {
+    		return (!(this.errorMsg===""));
+    	}
+    	this.isSuccess=function() {
+    		return (!(this.successMsg===""));
+    	}
+    	this.posalji= function() {
+    		this.errorMsg="";
+    		if (this.podaci.password.length < 5) this.errorMsg = "Password mora biti barem 5 karaktera dug!"
+    		else if (this.podaci.password != this.podaci.password_confirmation) this.errorMsg ="Password i potvrda passworda se razlikuju!";
+    		
+    		if (!(this.isError())) {
+    			res = $http.post('/students/add_student', this.podaci);
+				res.success(function(data, status, headers, config) {
+					if (data=="OK") this.successMsg = data;
+					else this.errorMsg=data;
+				});
+    		}
+    	}
+        
+    }]);
