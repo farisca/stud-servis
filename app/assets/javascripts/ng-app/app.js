94dacb53d0783ec80ac9742d4d4aeead8857cdc8
aibrahimovic <aibrahimov3@etf.unsa.ba>
2015-04-05 15:44:26 +0200

    Povezane forme

---
+++ app/assets/javascripts/ng-app/app.js
@@ -0,0 +1,66 @@
+/*angular
+    .module('aplikacija', [
+        'ngRoute',
+        'templates'
+    ]).config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/login', {
+                templateUrl: 'login.html',
+                controller: 'login'
+            });
+        $locationProvider.html5Mode(true);
+    });*/
+
+angular
+    .module('aplikacija', [
+        'ngRoute',
+        'templates'
+    ]).config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/registracija', {
+                templateUrl: 'registracija.html',
+                controller: 'register'
+            }).when('/login', {
+                templateUrl: 'login.html',
+                controller: 'login'
+            });
+        $locationProvider.html5Mode(true);
+    });
+
+
+/*angular
+    .module('aplikacija', [
+        'ngRoute',
+        'templates'
+    ]).config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'login.html',
+                controller: 'login'
+            }).whern ('/azra', {
+                templateUrl: 'login.html',
+                controller: 'login'
+            });
+        $locationProvider.html5Mode(true);
+    });*/
+
+
+/*angular
+    .module('aplikacija', [
+        'ngRoute',
+        'templates'
+    ]).config(function($routeProvider, $locationProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'login.html',
+        controller: 'login'
+      }).
+      when('/registracija', {
+        templateUrl: 'registracija.html',
+        controller: 'register'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);*/
+
---
+++ app/assets/javascripts/ng-app/controllers/registracija.js
@@ -0,0 +1,4 @@
+angular.module('aplikacija')
+    .controller('register', function ($scope) {
+        $scope.things = ['Angular', 'Rails 4.1', 'Working', 'Together!!'];
+    });
