fc5994b29904d714cbe857cd1f713be6d2828b06
Faris Cakaric <fcakaric1@etf.unsa.ba>
2015-04-29 11:11:54 +0200

    Ispravljen login

--- app/assets/javascripts/ng-app/services/AuthService.js
+++ app/assets/javascripts/ng-app/services/AuthService.js
@@ -18,21 +18,21 @@ angular.module('aplikacija').factory("AuthService", function($http, $q, $rootSco
       });
       return d.promise;
     }
-    confirm: function(tk, $scope) {
-      var d = $q.defer();
-      $http.post('/confirm_reg', {
-        tk: tk
-      }).success(function(resp) {
-        AuthToken.set(resp.auth_token, resp.type);
-        console.log("Uspjesno logiran. Dobiven token: " + resp.auth_token);
-        $location.path('/oglasi');
-        d.resolve(resp.user);
-      }).error(function(resp) {
-        console.log($rootScope);
-        $scope.errorMsg ="Netačni podaci!";
-        d.reject(resp.error);
-      });
-      return d.promise;
-    }
+    // confirm: function(tk, $scope) {
+    //   var d = $q.defer();
+    //   $http.post('/confirm_reg', {
+    //     tk: tk
+    //   }).success(function(resp) {
+    //     AuthToken.set(resp.auth_token, resp.type);
+    //     console.log("Uspjesno logiran. Dobiven token: " + resp.auth_token);
+    //     $location.path('/oglasi');
+    //     d.resolve(resp.user);
+    //   }).error(function(resp) {
+    //     console.log($rootScope);
+    //     $scope.errorMsg ="Netačni podaci!";
+    //     d.reject(resp.error);
+    //   });
+    //   return d.promise;
+    // }
   };
 });
