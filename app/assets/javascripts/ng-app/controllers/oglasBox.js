655119b64aba9ec85555067656183d2621368b40
ajdinkahrovic <kahrovic@hotmail.com>
2015-04-29 10:59:36 +0200

    angular direktiva za oglas box

---
+++ app/assets/javascripts/ng-app/controllers/oglasBox.js
@@ -0,0 +1,11 @@
+app=angular.module('aplikacija');
+
+app.directive('oglasBox', function () {
+	return {
+		restrict: 'E',
+		template: 'oglasBox.html',
+		link: function(scope, element, attrs) {
+
+		}
+	
+});
