f94d6355d1fa8cc5e3a14c12de9579b163ce9239
Faris Cakaric <fcakaric1@etf.unsa.ba>
2015-05-05 13:15:43 +0200

    Dodani neki prijevodi

--- app/assets/javascripts/ng-app/translate.js
+++ app/assets/javascripts/ng-app/translate.js
@@ -5,6 +5,18 @@ app.config(['$translateProvider', function ($translateProvider) {
     'STUDENT_SURNAME': 'Surname',
     'CITY': 'City',
     'MY_PROFILE': 'My profile',
+    'REGISTER_STUDENT': 'Register student',
+    'REGISTER_COMPANY': 'Register Company',
+    'MY_PROFILE': 'My profile',
+    'PROFILE': 'Profile',
+    'LOGIN': 'Login',
+    'LOGOUT': 'Logout',
+    'CATEGORIES': 'Categories',
+    'HOME': 'Home',
+    'COMPANIES': 'Companies',
+    'CHANGE_PASSWORD': 'Change password',
+    'AD': 'Ads',
+    'NEW_AD': 'New ad',
     'FOO': 'This is a paragraph'
   });
  
@@ -14,6 +26,18 @@ app.config(['$translateProvider', function ($translateProvider) {
     'STUDENT_SURNAME': 'Prezime',
     'CITY': 'Grad',
     'MY_PROFILE': 'Moj profil',
+    'REGISTER_STUDENT': 'Registracija studenta',
+    'REGISTER_COMPANY': 'Registracija kompanije',
+    'MY_PROFILE': 'Moj profil',
+    'PROFILE': 'Profil',
+    'LOGIN': 'Prijava',
+    'LOGOUT': 'Odjava',
+    'CATEGORIES': 'Kategorije',
+    'HOME': 'Doma doma',
+    'COMPANIES': 'Kompanije',
+    'CHANGE_PASSWORD': 'Promijeni šifru',
+    'AD': 'Oglasi',
+    'NEW_AD': 'Novi oglas',
     'FOO': 'Dies ist ein Absatz'
   });
  
