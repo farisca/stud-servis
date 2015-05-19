c53680d5959212a632c550424b7ba614d62612ba
Tajma Kovacevic <tajmakovacevic5@gmail.com>
2015-05-19 20:19:29 +0000

    Locations add Admin 1.2-Form established

--- app/assets/javascripts/ng-app/controllers/AdminLocationsCtrl.js
+++ app/assets/javascripts/ng-app/controllers/AdminLocationsCtrl.js
@@ -1,5 +1,13 @@
 app.controller("AdminLocationsCtrl", ['$scope', '$http', '$window', '$location', 'AuthToken', '$translate', function($scope, $http, $window, $location, AuthToken, $translate) {
     
+        $scope.data={};
+    	this.errorMsg="";
+    	this.successMsg="";
+	    var res;
+    	this.createNewLoc= function() {		
+		
+				
+		}
 }]);
 
 
