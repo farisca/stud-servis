3c302e5695edc749bd4b6f7f648a0ec6ffe17790
Tajma Kovacevic <tajmakovacevic5@gmail.com>
2015-05-19 23:29:48 +0000

    Statistike,obrisan drugi data set

--- app/assets/javascripts/ng-app/controllers/AdminStatisticsCtrl.js
+++ app/assets/javascripts/ng-app/controllers/AdminStatisticsCtrl.js
@@ -177,16 +177,6 @@ app.controller("AdminStatisticsCtrl", ['$scope', '$http', '$window', '$location'
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
             data: [65, 59, 90, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 96, 27, 100]
         }
     ]
 };
@@ -265,14 +255,6 @@ $scope.drawBarChart= function(){
             highlightFill: "rgba(220,220,220,0.75)",
             highlightStroke: "rgba(220,220,220,1)",
             data: [32, 15, 34, 4, 16, 33, 11]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.5)",
-            strokeColor: "rgba(151,187,205,0.8)",
-            highlightFill: "rgba(151,187,205,0.75)",
-            highlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
         }
     ]
 };
