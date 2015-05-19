26d545b0a768bbd02b820566e2a78b21f49e7da6
Tajma Kovacevic <tajmakovacevic5@gmail.com>
2015-05-19 19:28:21 +0000

    Locations add Admin 1.0

---
+++ app/assets/javascripts/ng-app/controllers/AdminLocationsCtrl.js
@@ -0,0 +1,7 @@
+app.controller("AdminLocationsCtrl", ['$scope', '$http', '$window', '$location', 'AuthToken', '$translate', function($scope, $http, $window, $location, AuthToken, $translate) {
+
+    
+}]);
+
+
+
