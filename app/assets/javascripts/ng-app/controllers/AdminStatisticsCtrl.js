1cb25060cc9d479637d51a351591036037ca60eb
Tajma Kovacevic <tajmakovacevic5@gmail.com>
2015-05-19 23:22:46 +0000

    Statistike, drga i zadnja-staticke

--- app/assets/javascripts/ng-app/controllers/AdminStatisticsCtrl.js
+++ app/assets/javascripts/ng-app/controllers/AdminStatisticsCtrl.js
@@ -3,6 +3,7 @@ app.controller("AdminStatisticsCtrl", ['$scope', '$http', '$window', '$location'
 	var signedUpUsersChart = document.getElementById("signedUpUsersChart").getContext("2d");
     var adsLocationChart = document.getElementById("adsLocationChart").getContext("2d");
     var registrationsChart = document.getElementById("registrationsChart").getContext("2d");
+    var categoriesChart = document.getElementById("categoriesChart").getContext("2d");
 
     // Neka pocetna inicijalizacija
     if ($translate.use() === 'en') {
@@ -164,48 +165,157 @@ app.controller("AdminStatisticsCtrl", ['$scope', '$http', '$window', '$location'
 
     //Funkcija koja crta grafig broj prijavljanih oglasa po odabranoj lokaciji
     $scope.drawAdsLocation = function() {
+        var data = {
+    labels: ["Mostar", "Sarajevo", "Zenica", "Tuzla", "Neum", "Bihac", "Jajce"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 90, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 96, 27, 100]
+        }
+    ]
+};
+var options=
+    {
+    //Boolean - Whether to show lines for each scale point
+    scaleShowLine : true,
 
+    //Boolean - Whether we show the angle lines out of the radar
+    angleShowLineOut : true,
 
+    //Boolean - Whether to show labels on the scale
+    scaleShowLabels : false,
 
-        //poziv servisa koji na osnovu proslijedjene lokacije vraca broj korisnika
-        var result;
-        var location_id = $scope.ads_locations.location_id;
+    // Boolean - Whether the scale should begin at zero
+    scaleBeginAtZero : true,
 
-        result = $http({ url: '/jobs/get_jobs_at_location', 
-            method: "GET",
-            params: {location_id: location_id}
-        });
+    //String - Colour of the angle line
+    angleLineColor : "rgba(0,0,0,.1)",
 
-        result.success(function(data, status, headers, config) {
-            var numberOfAds = data.number;
-            var labels = [];
-            alert("Broj oglasa: "+data.number);
+    //Number - Pixel width of the angle line
+    angleLineWidth : 1,
 
-            labels.push("1");
-            labels.push("2");
-            var adsLocationData = {
-                labels: labels,
-                datasets: [
-                    {
-                        label: "Ads on locaction",
-                        fillColor: "rgba(220,220,220,0.2)",
-                        strokeColor: "rgba(220,220,220,1)",
-                        pointColor: "rgba(220,220,220,1)",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(220,220,220,1)",
-                        data: numberOfAds
-                    }
-                ]
-            };
-            new Chart(adsLocationChart).Line(adsLocationData);
+    //String - Point label font declaration
+    pointLabelFontFamily : "'Arial'",
 
+    //String - Point label font weight
+    pointLabelFontStyle : "normal",
 
-        });    
+    //Number - Point label font size in pixels
+    pointLabelFontSize : 10,
+
+    //String - Point label font colour
+    pointLabelFontColor : "#666",
+
+    //Boolean - Whether to show a dot for each point
+    pointDot : true,
+
+    //Number - Radius of each point dot in pixels
+    pointDotRadius : 3,
+
+    //Number - Pixel width of point dot stroke
+    pointDotStrokeWidth : 1,
+
+    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+    pointHitDetectionRadius : 20,
+
+    //Boolean - Whether to show a stroke for datasets
+    datasetStroke : true,
+
+    //Number - Pixel width of dataset stroke
+    datasetStrokeWidth : 2,
+
+    //Boolean - Whether to fill the dataset with a colour
+    datasetFill : true,
+
+    //String - A legend template
+    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+}
+
+        
+new Chart(adsLocationChart).Radar(data, options);  
     }
 
 
+$scope.drawBarChart= function(){
+    
+    var data = {
+    labels: ["IT", "Medicine", "Farmacy", "Economy", "Psihology", "Cosmetics", "Consulting"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.5)",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "rgba(220,220,220,0.75)",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [32, 15, 34, 4, 16, 33, 11]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.5)",
+            strokeColor: "rgba(151,187,205,0.8)",
+            highlightFill: "rgba(151,187,205,0.75)",
+            highlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
 
+var options={
+    //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+    scaleBeginAtZero : true,
+
+    //Boolean - Whether grid lines are shown across the chart
+    scaleShowGridLines : true,
+
+    //String - Colour of the grid lines
+    scaleGridLineColor : "rgba(0,0,0,.05)",
+
+    //Number - Width of the grid lines
+    scaleGridLineWidth : 1,
+
+    //Boolean - Whether to show horizontal lines (except X axis)
+    scaleShowHorizontalLines: true,
+
+    //Boolean - Whether to show vertical lines (except Y axis)
+    scaleShowVerticalLines: true,
+
+    //Boolean - If there is a stroke on each bar
+    barShowStroke : true,
+
+    //Number - Pixel width of the bar stroke
+    barStrokeWidth : 2,
+
+    //Number - Spacing between each of the X value sets
+    barValueSpacing : 5,
+
+    //Number - Spacing between data sets within X values
+    barDatasetSpacing : 1,
+
+    //String - A legend template
+    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+}
+
+
+ new Chart(categoriesChart).Bar(data, options);
+}
     
 }]);
 
