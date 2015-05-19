14a9308d173411582940f6414ba54516db31aea9
Tajma Kovacevic <tajmakovacevic5@gmail.com>
2015-05-19 20:54:06 +0000

    Categories add Admin 1.0

---
+++ app/assets/javascripts/ng-app/controllers/AdminCategoriesCtrl.js
@@ -0,0 +1,6 @@
+app.controller("AdminCategoriesCtrl", ['$scope', '$http', '$window', '$location', 'AuthToken', '$translate', function($scope, $http, $window, $location, AuthToken, $translate) {
+   
+}]);
+
+
+
