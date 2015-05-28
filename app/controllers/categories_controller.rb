6bb85aaef5cd4386daf957e6fd9b81a076d9146f
Faris Cakaric <fcakaric1@etf.unsa.ba>
2015-05-28 22:56:45 +0200

    Refaktoring.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,45 +1,35 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
-  # GET /categories/1
-  # GET /categories/1.json
+  ##################################################################################################################
+  # GETTERS AND SETTERS
+  ##################################################################################################################
+  # GET all categories
   def getAllCategories
     render json: Category.all
   end
+
+  # GET a category
   def show
     @category = Category.find(params[:id])
     render json: @category
   end
 
-  # POST /categories
-  # POST /categories.json
+  # CREATE a category
   def add_category
-     u = Category.find_by(name: params["name"])
+    u = Category.find_by(name: params["name"])
     if !u.nil?
       return render json: { error: "Category already exists!" } 
     else
       
     category = Category.new
-    category.name=params["name"]
+    category.name = params["name"]
     category.save
     return render json: { error: "OK" }
   end
   end
 
-  # PATCH/PUT /categories/1
-  # PATCH/PUT /categories/1.json
-  def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /categories/1
-  # DELETE /categories/1.json
+  # DESTROY category
   def destroy
     @category.destroy
     respond_to do |format|
@@ -47,6 +37,9 @@ class CategoriesController < ApplicationController
     end
   end
 
+  ##################################################################################################################
+  # PRIVATE
+  ##################################################################################################################
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_category
